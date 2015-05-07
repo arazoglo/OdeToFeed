9df330c22e1c501b533582925837402b80a4f88a
arazoglo <arazoglo@hotmail.com>
2015-05-07 17:20:57 +0100

    Initial commit

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Infrastructure;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]
+    public class CuisineController : Controller
+    {
+        [HttpGet]
+        public ActionResult Search(string name="*")
+        {
+            throw new Exception("oops!");
+            //if (name == "*")
+            //{
+            //    return Json(new { cusinineName = name }, JsonRequestBehavior.AllowGet);
+            //    //return File(Server.MapPath("~/Content/Site.css"), "text/css");
+            //    //return RedirectToRoute("Cuisine", new { name = "german" });
+            //    //return RedirectToAction("Search", "Cuisine", new { name = "french" });
+            //}
+            name = Server.HtmlEncode(name);
+
+            //return RedirectToAction("Index", "home");
+            return Content(name);
+        }
+
+    }
+}
---
+++ OdeToFood/Models/OdeToFoodDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFood.Models
+{
+    public class OdeToFoodDb
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> Reviews { get; set; }
+
+    }
+}
