a35b98370931d418fd3219d12d7147d0a4f529f0
arazoglo <arazoglo@hotmail.com>
2015-05-20 17:09:36 +0100

    Final commit - IoC and database decoupled

---
+++ OdeToFood.Tests/ReviewControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Tests
+{         
+    [TestFixture]
+    public class ReviewControllerTests
+    {
+        [Test]
+        public void Index_Action_Model_Is_Three_Review()
+        {
+            var db = new FakeDbContext();
+
+            db.Reviews = new[]{
+                new Review(),
+                new Review(),
+                new Review(),
+                new Review(),
+            }.AsQueryable();
+
+            var controller = new ReviewController(db);
+            var result = controller.Index() as ViewResults;
+            var model = result.Model as IEnumerable<Review>;
+
+            Assert.AreEqual(3, model.Count());
+        }
+
+
+    }
+}
